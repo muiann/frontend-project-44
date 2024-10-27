288616e2f6bcf5ce4feb2762f6f7ed70296ea2c0
Анна <anna.dyatlova.ad@gmail.com>
2024-10-27 17:53:09 +0500

    chande brain-games.js, cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 
 import readlineSync from 'readline-sync';
-
+console.log('Welcome to the Brain Games!');
+let userName = readlineSync.question('May I have your name? ');
 const getUserName = () => {
 
- let userName = readlineSync.question('May I have your name? ');
   console.log('Hello ' + userName + '!');
 };
 export default getUserName;
